c66e68238ff8fc0a72e5044b33bb816e46497219
Steve Shipway <steve.shipway@smxemail.com>
2022-05-19 11:07:13 +1200

    Update Thu 19 May 2022 11:07:13 NZST

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo Update git
+git add .
+git commit -m "Update $(date)"
+git push
+echo Mcollective redeploy
+mco r10k deploy steves
+for i in az1pca01 az1ppm01 az1ppm02
+do
+  echo restart $i
+  ssh $i sudo systemctl restart puppetmaster
+done
+echo Running test
+ssh xmdwfd01 sudo puppet agent -t --no-noop --tags op
+
